bb3811d109c040597ff77095d24c25919ee27a88
murali <murali@gmail.com>
2022-10-10 12:43:26 +0530

    Add project files.

---
+++ MVCAuthentication/Controllers/AuthenticateController.cs
@@ -0,0 +1,46 @@
+﻿using MVCAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCAuthentication.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        // GET: Authenticate
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using(authenticateEntities db = new authenticateEntities())
+            {
+                var result =  db.tblUserMasters.Where(x => x.UserName == user.Username && x.Password == user.Password);
+
+                if(result.Count() > 0)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["msg"] = "Incorrect login details";
+                }
+            }
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return View("Login");
+        }
+    }
+}
